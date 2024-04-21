a2433174d5c592f129c8d309982e833a4e062b2c
TIMUR CHELIK <slandarka22@gmail.com>
2024-04-21 17:52:03 +0300

    added youtube player (YouTube API) to the page

--- dist/script.js
+++ dist/script.js
@@ -18,12 +18,40 @@ class VideoPlayer {
     this.overlay = document.querySelector(overlay);
     this.close = this.overlay.querySelector('.close');
   }
-  play() {
+  bindTriggers() {
     this.btns.forEach(btn => {
       btn.addEventListener('click', () => {
-        console.log('hello world');
+        const path = btn.getAttribute('data-url');
+        if (!this.player) this.createPlayer(path);else this.overlay.style.display = 'flex';
       });
     });
+  }
+  bindCloseBtn() {
+    this.close.addEventListener('click', () => {
+      this.player.stopVideo();
+      this.overlay.style.display = 'none';
+    });
+    this.overlay.addEventListener('click', () => {
+      this.player.stopVideo();
+      this.overlay.style.display = 'none';
+    });
+  }
+  createPlayer(url) {
+    this.player = new YT.Player('frame', {
+      height: '360',
+      width: '640',
+      videoId: `${url}`
+    });
+    console.log(this.player);
+    this.overlay.style.display = 'flex';
+  }
+  init() {
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    this.bindTriggers();
+    this.bindCloseBtn();
   }
 }
 
@@ -162,7 +190,7 @@ window.addEventListener('DOMContentLoaded', () => {
   const slider = new _modules_slider__WEBPACK_IMPORTED_MODULE_0__["default"]('.page', '.next');
   slider.render();
   const player = new _modules_playVideo__WEBPACK_IMPORTED_MODULE_1__["default"]('.showup .play', '.overlay');
-  player.play();
+  player.init();
 });
 })();
 
--- src/js/modules/playVideo.js
+++ src/js/modules/playVideo.js
@@ -5,11 +5,47 @@ export default class VideoPlayer {
     this.close = this.overlay.querySelector('.close');
   }
 
-  play() {
+  bindTriggers() {
     this.btns.forEach(btn => {
       btn.addEventListener('click', () => {
-        console.log('hello world');
+        const path = btn.getAttribute('data-url');
+        if (!this.player) this.createPlayer(path);
+        else this.overlay.style.display = 'flex';
       });
     });
+  }
+
+  bindCloseBtn() {
+    this.close.addEventListener('click', () => {
+      this.player.stopVideo();
+      this.overlay.style.display = 'none';
+    });
+
+    this.overlay.addEventListener('click', () => {
+      this.player.stopVideo();
+      this.overlay.style.display = 'none';
+    });
+  }
+
+  createPlayer(url) {
+    this.player = new YT.Player('frame', {
+      height: '360',
+      width: '640',
+      videoId: `${url}`,
+    });
+    console.log(this.player);
+
+    this.overlay.style.display = 'flex';
+  }
+
+  init() {
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    this.bindTriggers();
+    this.bindCloseBtn();
   }
 }
